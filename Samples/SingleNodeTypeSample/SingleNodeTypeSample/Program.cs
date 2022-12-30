1937023dc661c74fa4b3c1292f468baf50444c5a
Jeff Scherrer <jeff@mushmo.com>
2022-12-30 03:46:55 -0800

    Initial check-in.

---
+++ Samples/SingleNodeTypeSample/SingleNodeTypeSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using MQTTnet.AspNetCore;
+
+var builder = new HostBuilder();
+
+builder.UseOrleans(silo =>
+{
+    silo.UseLocalhostClustering();
+});
+
+builder.Services
+    .AddMqttServer(options =>
+    {
+        options.WithDefaultEndpoint();
+    })
+    .AddMqttServerOrleansClustering();
+
+var host = builder.Build();
+
+await host.RunAsync();
